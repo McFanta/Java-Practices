f8bca950f13713101522b0829a3163ad53c3315a
mcfanta <johannes.theigen@gmail.com>
2016-10-06 21:09:41 +0200

    Push some files

---
+++ src/Pinum.java
@@ -0,0 +1,10 @@
+public class Pinum {
+    static String  randomString = "String to print";
+
+    static final double PINUM = 3.141429;
+
+    public static void main(String[] args)
+    {
+        System.out.println(PINUM);
+    }
+}
---
+++ src/variable.java
@@ -0,0 +1,9 @@
+public class variable {
+    public static void main(String[] args)
+    {
+        int integerOne = 22;
+        int integerTwo = integerOne + 1;
+
+        System.out.println(integerTwo);
+    }
+}
