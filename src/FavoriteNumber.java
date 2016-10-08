a9bff298df3bb94ecffc80522bdf270eb797fef2
mcfanta <johannes.theigen@gmail.com>
2016-10-08 20:22:24 +0200

    Add a FavoriteNumber program and a push some other things

---
+++ src/FavoriteNumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class FavoriteNumber {
+    static Scanner userInput = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+
+       System.out.print("Your favorite number: ");
+
+        if (userInput.hasNextInt())
+        {
+
+            int numberEntered = userInput.nextInt();
+
+            System.out.println("You entered " + numberEntered);
+
+        }
+
+    }
+}
