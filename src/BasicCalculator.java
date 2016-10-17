efa938f7a02034b313eeff9afe576ef770951328
McFanta <johannes.theigen@gmail.com>
2016-10-17 20:32:36 +0200

    Add a basic calculator

---
+++ src/BasicCalculator.java
@@ -0,0 +1,11 @@
+public class BasicCalculator {
+
+    public static void main(String[] args) {
+
+        int num1 = 234;
+        int num2 = 400;
+        int answer = num1 + num2;
+
+        System.out.println(answer);
+    }
+}
--- src/dialog.java
+++ src/dialog.java
@@ -5,4 +5,3 @@ public class dialog {
         showMessageDialog(null, "Hello " + name);
     }
 }
-
